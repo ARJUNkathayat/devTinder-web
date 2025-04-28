e57d3c776945351ea74f15a8988469ec8d2ef3a5
Arjun <kingarjun74280@gmail.com>
2025-04-28 20:36:07 +0530

    Routing

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # create devtinder frontend
 react+ vite
 remove uneccesary files
+install tailwind ,daisy ui
+take navbar component from daisy 
+install react router dom
+create routes ,parent and children routes
+create outlet in body component
+create footer
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 // src/App.jsx
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
+import Body from './Body';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Profile from './Profile';
+import Login from './Login';
 function App() {
   return (
     <>
-  <Navbar/>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element = {<Body/>}>
+    <Route path="/login" element = {<Login/>}/>
+    <Route path="/profile" element = {<Profile/>}/>
+    
+    </Route>
+  </Routes>
+  
+  </BrowserRouter>
     </>
   )
 }
